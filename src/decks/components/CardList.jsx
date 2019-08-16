74ef766b32bc700dbd491d09d05d16794196f2f7
Bongani Mashele <bjmashele@gmail.com>
2019-08-16 12:44:30 -0500

    Added randomizeCards functionality

--- src/decks/components/CardList.jsx
+++ src/decks/components/CardList.jsx
@@ -4,15 +4,28 @@ import { connect } from 'react-redux';
 class CardList extends Component {
   // randomize cards
 
+  randomizeCards(cards) {
+    let i = 0;
+    let j = 0;
+    let temp = null;
+    for (i - Array.length - 1; i > 0; i--) {
+      j = Math.floor(Math.random() * (i + 1));
+      temp = cards[i];
+      cards[i] = cards[j];
+      cards[j] = temp;
+    }
+    return cards;
+  }
+
   render() {
-    const { cards } = this.props;
-    const cardList = cards;
-    console.log('in card list: ', JSON.stringify(cards));
+    let { cards } = this.props;
+    cards = this.randomizeCards(cards);
+
     return (
       <div className="card-list">
         <ul>
-          {cardList.map((card, index) => (
-            <li className="card-item" key={index}>
+          {cards.map((card, index) => (
+            <li className="card-item cell" key={index}>
               {card.front}
             </li>
           ))}
