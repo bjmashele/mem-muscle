e1dcf0e192ef366bc32cd8d7c161e22867e24727
Bongani Mashele <bjmashele@gmail.com>
2019-10-26 22:50:01 -0500

    Pointed the decks api to heroku app

--- client/src/api/decksApi.js
+++ client/src/api/decksApi.js
@@ -1,20 +1,10 @@
 import { HttpClient } from "./httpClient";
 
-// This is the API. The backend root URL can be set from here.
+const DECKS_API = "https://limitless-river-84405.herokuapp.com/api/decks";
 
-//const API = "api/decks";
-
-// Setting the decks URI
-
-// const DECKS_API = `${API}/decks`;
-// Use json-server
-const DECKS_API = "/api/decks";
-//const DECKS_API = "/api/decks";
-// Get card decks
 const fetchDecks = () => HttpClient.get(DECKS_API);
 const addDeck = data => HttpClient.post(DECKS_API, data);
 const addCard = card => {
-  console.log("In add card api: card  :", card);
   return HttpClient.put(`${DECKS_API}/addCard`, card);
 };
 
