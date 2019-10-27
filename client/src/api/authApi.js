2a496861b989b71fa3db938583fae24f2e6b3420
Bongani Mashele <bjmashele@gmail.com>
2019-10-26 21:43:19 -0500

    Fixed an error on auth api url string

--- client/src/api/authApi.js
+++ client/src/api/authApi.js
@@ -2,8 +2,8 @@ import { HttpClient } from "./httpClient";
 
 // This is the API. The backend root URL can be set from here.
 
-const AUTH_API = "http://localhost:3030/api/auth";
-const AUTH_API = "https://limitless-reef-91192.herokuapp.com";
+//const AUTH_API = "http://localhost:3030/api/auth";
+const AUTH_API = "https://limitless-reef-91192.herokuapp.com/api/auth";
 //const AUTH_API = `api/auth`;
 
 //signup
